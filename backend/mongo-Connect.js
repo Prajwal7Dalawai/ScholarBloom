214cfabbfffc41a7a443a72a045dca9804504a0e
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2025-03-09 16:11:58 +0530

    Changes in mongo-Connect

--- backend/mongo-Connect.js
+++ backend/mongo-Connect.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+let URL = "mongodb://127.0.0.1:27017/ScholarBloom";
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB Connected");
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
+
+const connectDB = async()=>{
+    try{
+    await mongoose.connect(URL);
+    console.log("Connected to Database");
+}
+catch(error){
+    console.log(error.message)
+    console.log(e);
+    process.exit(1);
+}
 };
-
+connectDB();
 module.exports = connectDB;
