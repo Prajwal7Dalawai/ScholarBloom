2b67063892d6af45499aebc8c1ec988f04d08a94
student-bhavanamh <bhavanahottigoudar@gmail.com>
2025-03-26 08:30:22 +0530

    Changes done

--- frontend/ScholarBloom/src/App.jsx
+++ frontend/ScholarBloom/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
+// import './App.css'
 import Landing from './pages/Landing'
 import SignupPage from './pages/SignupPage.jsx'
 import StudentDashboard from './pages/StudentDashboard'
